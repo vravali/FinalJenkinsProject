920068183f378df8e5886b2fa17f4a683394d2de
vravali <vangav@brandeis.edu>
2018-09-11 16:49:37 -0400

    2nd Attempt

--- FinalJenkinsProject/src/edu/vvangala/jenkins/code/Prime1Test.java
+++ FinalJenkinsProject/src/edu/vvangala/jenkins/code/Prime1Test.java
@@ -1,20 +1,18 @@
-package testing;
+package edu.vvangala.jenkins.code;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import edu.vvangala.jenkins.code.SieveOfEratosthenes;
-
-public class Prime3Test {
+public class Prime1Test {
 
 	@Test
 	public void testPrimesList() {
-		//Test3
-		int b=66;
+		//Test1
+		int b=10;
 		SieveOfEratosthenes x = new SieveOfEratosthenes();
 		String output = SieveOfEratosthenes.p(SieveOfEratosthenes.primesList(x.Sieve(b),SieveOfEratosthenes.primesCount(x.Sieve(b))));
-		String expectedOutput = ("[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]");
+		String expectedOutput = ("[2, 3, 5, 7]");
 		assertEquals(expectedOutput, output);
 	}
 
--- FinalJenkinsProject/src/edu/vvangala/jenkins/code/Prime3Test.java
+++ FinalJenkinsProject/src/edu/vvangala/jenkins/code/Prime3Test.java
@@ -1,20 +1,18 @@
-package testing;
+package edu.vvangala.jenkins.code;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import edu.vvangala.jenkins.code.SieveOfEratosthenes;
-
-public class Prime2Test {
+public class Prime3Test {
 
 	@Test
 	public void testPrimesList() {
-		//Test2
-		int b=75;
+		//Test3
+		int b=66;
 		SieveOfEratosthenes x = new SieveOfEratosthenes();
 		String output = SieveOfEratosthenes.p(SieveOfEratosthenes.primesList(x.Sieve(b),SieveOfEratosthenes.primesCount(x.Sieve(b))));
-		String expectedOutput = ("[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73]");
+		String expectedOutput = ("[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]");
 		assertEquals(expectedOutput, output);
 	}
 
